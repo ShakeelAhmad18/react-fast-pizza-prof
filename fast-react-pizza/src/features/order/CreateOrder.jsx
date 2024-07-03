ad4ee88054485637cc84681648bc40e729271b3f
Shakeel Ahmad <150515418+ShakeelAhmad18@users.noreply.github.com>
2024-07-03 16:23:26 +0500

    make some changes

--- fast-react-pizza/src/features/order/CreateOrder.jsx
+++ fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -43,22 +43,27 @@ function CreateOrder() {
 
     return (
         <div>
-            <h2 className='my-3'>Ready to order? Let's go</h2>
+            <h2 className='py-3 px-6 text-xl font-semibold'>Ready to order? Let's go</h2>
             <Form method='POST'>
-               <label>Name</label>
-                <div>
-                    <input className='input' type="text" name="customer" required />
+                <div className='mb-5 flex flex-col gap-2 sm:flex-row sm:items-center'>
+                 <label className='sm:basis-40'>First Name</label>
+                 <input className='input grow' type="text" name="customer" required />
                 </div>
-                <label>Phone Number</label>
-                <div>
-                    <input className='input' type="tel" name="phone" required/>
+                 <div className='mb-5 flex flex-col gap-2 sm:flex-row sm:items-center'>
+                   <label className='sm:basis-40'>Phone Number</label>
+                 <div className='grow'>
+                    <input className='input w-full' type="tel" name="phone" required/>
+                    {formError?.phone && <p className='mt-2 rounded-md bg-red-100 text-xs text-red-700'>{formError.phone}</p>}
+                 </div>
                 </div>
-                {formError?.phone && <p>{formError.phone}</p>}
-                <label>Address</label>
-                <div>
-                    <input className='input' type="text" name="address" required />
+           
+                <div className='mb-5 flex flex-col gap-2 sm:flex-row sm:items-center'>
+                  <label className='sm:basis-40'>Address</label>
+                  <div className='grow'>
+                    <input className='input w-full' type="text" name="address" required />
+                  </div>
                 </div>
-                <div>
+                <div className='mb-12 flex items-center gap-5'>
                     <input className='my-3 mx-2 h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-offset-2 focus:ring-yellow-400' type="checkbox" name="priority" id="priority" />
                     <label htmlFor="priority">Want to you give order Priority</label>
                 </div>
--- fast-react-pizza/src/ui/Button.jsx
+++ fast-react-pizza/src/ui/Button.jsx
@@ -4,7 +4,7 @@ function Button({children,disabled,to,type}) {
   
      const styles={
        primary:'bg-yellow-400 tracking-wide rounded-full text-stone-800 py-2 px-3 font-semibold duration-500 inline-block uppercase focus:bg-yellow-300 focus:ring focus:outline-none focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed md:py-4 md:px-6',
-       base:'bg-stone-400 tracking-wide rounded-full text-stone-800 py-2 px-3 font-semibold duration-500 inline-block uppercase focus:bg-stone-300 focus:ring focus:outline-none focus:ring-stone-300 focus:ring-offset-2 disabled:cursor-not-allowed md:py-4 md:px-6'
+       base:'bg-stone-200 tracking-wide rounded-full text-stone-800 py-2 px-3 font-semibold duration-500 inline-block uppercase focus:bg-stone-400 focus:ring focus:outline-none focus:ring-stone-300 focus:ring-offset-2 disabled:cursor-not-allowed md:py-4 md:px-6'
      }
      
      if(to)
